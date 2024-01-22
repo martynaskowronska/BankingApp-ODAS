f0bb6ed7b4890d385cfc8019d3b242e4842f826a
martynaskowronska <martyna.skowronska333@gmail.com>
2024-01-23 00:38:53 +0100

    authorization

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 import random
+from .models import User, Permutation
+from Crypto.Cipher import AES
+from dotenv import load_dotenv
+import os, hashlib
 
 auth = Blueprint('auth', __name__)
 
@@ -8,21 +12,72 @@ def gen_random_inputs(pswd_lenght):
     inputs = random.sample(range(pswd_lenght), no_of_inputs)
     return inputs
 
-# def verify_password(given_password):
+def decrypt_length(encrypted_data):
+    load_dotenv()
+    key = os.getenv("AES_KEY").encode('utf-8')
+
+    iv = encrypted_data[:16]
+    ciphertext = encrypted_data[16:]
+
+    decrypt_cipher = AES.new(key, AES.MODE_CFB, iv=iv)
+    decrypted_length_bytes = decrypt_cipher.decrypt(ciphertext)
+    decrypted_length = int.from_bytes(decrypted_length_bytes, byteorder='big')  # Remove padding
+
+    return decrypted_length
+
+def hash_password(password, salt):
+    hashed_password = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 500000)
+
+    return hashed_password.hex()
+
+def verify_password(provided_password, stored_password, salt):
+    hash_provided_password = hash_password(provided_password, salt)
+
+    print(hash_provided_password)
+    for i in range(0, len(stored_password), len(hash_provided_password)):
+        substring = stored_password[i:i+len(hash_provided_password)]
+        if substring == hash_provided_password:
+            return True
+        
+    return False
 
 @auth.route('/', methods=['GET', 'POST'])
 def client_number():
     if request.method == 'POST':
         client_number = request.form.get('client_number')
-        return redirect(url_for('auth.password'))
+        user = User.query.filter_by(client_number = client_number).first()
+        if user:
+            session['client_number'] = client_number
+            return redirect(url_for('auth.password'))
+        else:
+            flash('Wrong client number. Try again.', category = 'error')
     return render_template("client_number.html")
 
 @auth.route('/password', methods=['GET', 'POST'])
 def password():
+    user = User.query.filter_by(client_number = session['client_number']).first()
+    enc_pswd_lenght = user.password_length
+    pswd_length = decrypt_length(enc_pswd_lenght)
+
     if request.method == 'GET':
-        required_inputs = gen_random_inputs(6)
-        print (required_inputs)
-    return render_template("password.html", no_inputs=6, required_inputs = required_inputs)
+        session['non_required_inputs'] = gen_random_inputs(pswd_length)
+        return render_template("password.html", no_inputs=pswd_length, non_required_inputs = session['non_required_inputs'])
+    else:
+        provided_password = ''.join(request.form.get(f'input{i}') for i in range(pswd_length) if i not in session['non_required_inputs'])
+        print(provided_password)
+        salt = user.salt
+        permutations = Permutation.query.filter_by(client_number = session['client_number']).first()
+        if permutations:
+            stored_password = permutations.permutations
+            if verify_password(provided_password, stored_password, salt):
+                return redirect(url_for('views.home'))
+            else:
+                flash('Wrong password. Try again.', category='error')
+                return render_template("password.html", no_inputs=pswd_length, non_required_inputs = session['non_required_inputs'])
+        else:
+            flash('Something went wrog. Try again.', category='error')
+            return render_template("password.html", no_inputs=pswd_length, non_required_inputs = session['non_required_inputs'])
+
 
 @auth.route('/logout')
 def logout():
